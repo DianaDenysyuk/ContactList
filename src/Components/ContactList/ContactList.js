9db8d8fa9a0102b4138dfc2cd9544c5a2e32069b
Diana Denysyuk <didenysyuk@gmail.com>
2019-09-17 21:36:37 +0300

    add new components ContactListItem and change index.js & ContactList.js

--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,19 +1,14 @@
-import React from 'react'
+import React from 'react';
+
+import ContactListItem from '../ContactListItem/ContactListItem'
 
 const ContactList = () =>{
     return(
-        <li><div className="media">
-	            <div className="media-left align-self-center">
-	                <img className="rounded-circle" src="https://randomuser.me/api/portraits/women/50.jpg" alt=""/>
-	            </div>
-	            <div className="media-body">
-	                <h4>Camila Terry</h4>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-	            </div>
-	            <div className="media-right align-self-center">
-	                <a href="https://www.google.com" className="btn btn-default">Contact Now</a>
-	            </div>
-        </div></li>
+        <div className="col-md-10 offset-md-1 row-block">
+            <ul id="sortable">
+                <ContactListItem />          
+            </ul>
+        </div>
     )
 }
 export default ContactList;
--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import './index.css';
 import Header from './Components/Header/Header';
 import ContactList from './Components/ContactList/ContactList';
 
+const listOfContact = [
+    {
+        name : "Camila Terry",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+        gender : "women",
+        avatar : "https://randomuser.me/api/portraits/women/50.jpg"
+    }
+]
+console.log(listOfContact);
+
 const App = () => {
     return(
         <section className="row-section">
             <div className="continer">
                 <Header />
-                <div className="col-md-10 offset-md-1 row-block">
-                    <ul id="sortable">
-                        <ContactList />
-                    </ul>
-                </div>
+                <ContactList />
             </div>
         </section>
     )
